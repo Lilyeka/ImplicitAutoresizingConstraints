d81574b169ade42f8d3148907fae79e1d1a3a904
Liliya <lilyeka@gmail.com>
2019-04-12 13:41:21 +0500

    Initial Commit

---
+++ ImplicitAutoresizingConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImplicitAutoresizingConstraints
+//
+//  Created by Лилия Левина on 12/04/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
