7dbf375bff3b215416ce39d728a470edc0bf66a7
Liliya <lilyeka@gmail.com>
2019-04-12 14:44:01 +0500

    print constraints

--- ImplicitAutoresizingConstraints/ViewController.swift
+++ ImplicitAutoresizingConstraints/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let lab1 = UILabel(frame:CGRect(x:270,y:60,width:42,height:22))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let lab1 = UILabel(frame:CGRect(x:270,y:60,width:42,height:22))
+        
         lab1.autoresizingMask = [.flexibleLeftMargin, .flexibleBottomMargin]
         lab1.text = "Hello"
         self.view.addSubview(lab1)
@@ -26,18 +28,15 @@ class ViewController: UIViewController {
             lab2.trailingAnchor.constraint(
                 equalTo: self.view.trailingAnchor, constant: -20)
             ])
-        
-//  This causes the first label to be involved in autolayout. Therefore, the first label magically acquires four automatically generated implicit constraints of class NSAutoresizingMaskLayoutConstraint, such as to give the label the same size and position, and the same behavior when its superview is resized, that it had when it was configured by its frame and autoresizingMask:
-//        <NSAutoresizingMaskLayoutConstraint:0x6000002818b0 h=&-- v=--&
-//        UILabel:0x7f9d3820bf80'Hello'.midX == UIView:0x7f9d383079d0.width-29>
-//        <NSAutoresizingMaskLayoutConstraint:0x60000009fe50 h=&-- v=--&
-//        UILabel:0x7f9d3820bf80'Hello'.midY == 71>
-//        <NSAutoresizingMaskLayoutConstraint:0x60000009fef0 h=&-- v=--&
-//        UILabel:0x7f9d3820bf80'Hello'.width == 42>
-//        <NSAutoresizingMaskLayoutConstraint:0x6000002821c0 h=&-- v=--&
-//        UILabel:0x7f9d3820bf80'Hello'.height == 22>
+        //  This causes the first label to be involved in autolayout. Therefore, the first label magically acquires four automatically generated implicit constraints of class NSAutoresizingMaskLayoutConstraint, such as to give the label the same size and position, and the same behavior when its superview is resized, that it had when it was configured by its frame and autoresizingMask:
     }
 
+    override func viewDidLayoutSubviews() {
+    // this code shows automatically generated implicit constraints of class NSAutoresizingMaskLayoutConstraint
+        let arr1 = lab1.constraintsAffectingLayout(for:.horizontal)
+        let arr2 = lab1.constraintsAffectingLayout(for:.vertical)
+        let s = String(format: "\n\n%@\nH: %@\nV:%@", lab1, arr1, arr2)
+        print(s)
+    }
 
 }
-
